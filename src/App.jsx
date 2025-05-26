0c7c8be43b378881f4b4f2eb0886da8323a6743a
raushan <kraushan022@gmail.com>
2025-05-26 23:15:48 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Country from "./pages/Country";
+import Contact from "./pages/Contact";
+import AppLayout from "./components/Layout/AppLayout";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/country",
+        element: <Country />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>Countries App</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Country = () => {
-  return <div>Country</div>;
+  return <h1>Country</h1>;
 };
 
 export default Country;
