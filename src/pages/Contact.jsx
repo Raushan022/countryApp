23fad24f3f47dd8124d8113e11af5b4c8d81eb8d
raushan <kraushan022@gmail.com>
2025-05-27 15:12:46 +0530

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Your Name"
+            name="username"
+            required
+            autoComplete="off"
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter Your Email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+
+          <textarea
+            className="form-control"
+            rows={10}
+            placeholder="Enter Your Message"
+            name="message"
+            required
+            autoComplete="off"
+          ></textarea>
+
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
