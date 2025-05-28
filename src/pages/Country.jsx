b979f0f780db8741edfc5fb50c9a8e444e7cea6b
raushan <kraushan022@gmail.com>
2025-05-28 18:40:42 +0530

    search and filter UI

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/postApi";
 import Loader from "../components/UI/Loader";
 import CountryCard from "../components/Layout/CountryCard";
+import SearchFilter from "../components/UI/SearchFilter";
 
 const Country = () => {
   const [isPending, startTransition] = useTransition();
   const [countries, setCountries] = useState([]);
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     startTransition(async () => {
@@ -19,6 +23,12 @@ const Country = () => {
 
   return (
     <section className="country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+      />
       <ul className="grid grid-four-cols">
         {countries.map((country, index) => (
           <CountryCard key={index} country={country} />
